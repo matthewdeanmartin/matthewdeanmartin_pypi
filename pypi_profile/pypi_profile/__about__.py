7ba9b58aa6eb7ec8ddf2fc44f134c66f19b12187
Matthew Dean Martin <matthewdeanmartin@gmail.com>
2026-05-16 13:14:20 -0400

    0.2.0: nit picks

--- pypi_profile/pypi_profile/__about__.py
+++ pypi_profile/pypi_profile/__about__.py
@@ -14,8 +14,8 @@ __all__ = [
 ]
 
 __title__ = "pypi-profile"
-__version__ = "0.1.0"
-__description__ = "The missing PyPI(tm) profile page — pipx-installable, plugin-extensible. Not associated with PSF."
+__version__ = "0.2.0"
+__description__ = "The missing PyPI(tm) profile page — link your profile with signatures. Not associated with PSF."
 __readme__ = "README.md"
 __credits__ = [{"name": "Matthew Martin", "email": "matthewdeanmartin@gmail.com"}]
 __keywords__ = ["pypi", "profile", "cli"]
